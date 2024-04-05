db02ed7e57ec71776816d38afaf968fbf327ef00
Comorina <shivampandey91198@gmail.com>
2024-04-05 18:32:18 +0530

    add service page design

---
+++ src/app/pages/Services/serviceModel.ts
@@ -0,0 +1,36 @@
+export const serviceData = [
+    {
+        serviceLabelTitle: 'SERVICES',
+        serviceHeadingTitle: 'We Offer <br/><strong>Best solutions</strong><br/> for you',
+        serviceSubHeadingTitle: 'We provide digital experience services to startups and small businesses to looking for a partner of their digital media, design & development, lead generation and communications requirents. We work with you, not for you. Although we have a great resources',
+        button: [
+            {
+                action: '',
+                title: 'SEE OUR WORKS'
+            }
+        ],
+        cardComponent:[
+            {
+                image: '',
+                cardHeading: 'Design & Development',
+                cardContent: 'UI/UX, Poster, Visiting Cards, Flyers, logos, Static/Dynamic Websites, Blogs, E-Commerce, Chat Bots, Forums, API Integrations, POS Systems and more..'
+            },
+            {
+                image: '',
+                cardHeading: 'Digital Marketing and IT Consulting',
+                cardContent: 'We are here to accompany you in IT strategy development and with our expert digital marketing strategies your business will scale-up to a new level of market competitiveness and you will get the most from your investments.'
+            },
+            {
+                image: '',
+                cardHeading: 'Cloud Services & Maintenance',
+                cardContent: 'Domain, SSL - Standard/ Premium/ Wildcard, Hosting - Managed/ Shared/ Dedicated, VPS - Shared/ Dedicated, Docker Application Hosting, AMC'
+            },
+            
+            {
+                image: '',
+                cardHeading: 'Power API & Integrations',
+                cardContent: 'Easy and Ready-to-Use APIs for GST, Trademark Status, CIN Information and New Companies, Custom APIs and Scrapers, Building and Integration.'
+            }
+        ]
+    }
+]
--- src/app/pages/Services/servicesComponent.tsx
+++ src/app/pages/Services/servicesComponent.tsx
@@ -1,10 +1,46 @@
 import { Container } from "react-bootstrap";
 import '../Services/services.css'
+import { serviceData } from "./serviceModel";
 function ServicesComponent(){
+    const data = serviceData[0]
     return(
         <div id="services">
             <Container className="services-main-container">
-                Hello Services
+                <div className="heading-title-container">
+                    <div className="service-label">
+                        {data.serviceLabelTitle}
+                    </div>
+                    <div className="service-heading-title">
+                        <p dangerouslySetInnerHTML={{ __html: data.serviceHeadingTitle }}></p>
+                    </div>
+                    <div className="servic-subHeading-title">
+                        <p>
+                            {data.serviceSubHeadingTitle}
+                        </p>
+                    </div>
+                    <div className="service-button-container">
+                        <button className="our-works">
+                            {data.button[0]['title']}
+                        </button>
+                    </div>
+                </div>
+                <div className="service-card-container">
+                    {data.cardComponent.map((item:any, index:any)=>{
+                        return(
+                            <div key={index} className="service-card">
+                                <div className="service-card-image">
+
+                                </div>
+                                <div className="service-card-heading-title">
+                                    <p>{item['cardHeading']}</p>
+                                </div>
+                                <div className="service-card-content">
+                                    <p>{item['cardContent']}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </Container>
         </div>
     )
